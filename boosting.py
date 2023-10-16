38a080bbdd7d4fd1ba7aaf7847a1718787a27f31
James-Wukong <qianfan.cheng.cn@gmail.com>
2023-10-16 01:57:41 -0400

    add boosting and stacking
    1. add boosting implementation
    2. add stacking implementaion

---
+++ boosting.py
@@ -0,0 +1,47 @@
+import numpy as np
+from sklearn.datasets import make_classification, make_regression
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold, RepeatedKFold
+from matplotlib import pyplot
+
+# get the classification dataset
+def get_dataset(data_type='classification'):
+    if data_type == 'classification':
+        X, y = make_classification(n_samples=1000, n_features=10, n_informative=5, n_redundant=5, random_state=1)
+    elif data_type == 'regression':
+         X, y = make_regression(n_samples=1000, n_features=10, n_informative=5, noise=0.1, random_state=1)
+    return X, y
+
+# define dataset
+X, y = get_dataset()
+
+# Gradient Boosting Machine for Classification
+# evaluate the model
+model = GradientBoostingClassifier()
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+n_scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+print('Accuracy: %.3f (%.3f)' % (np.mean(n_scores), np.std(n_scores)))
+
+# fit the model on the whole dataset
+model = GradientBoostingClassifier()
+model.fit(X, y)
+# make a single prediction
+row = [[2.56999479, -0.13019997, 3.16075093, -4.35936352, -1.61271951, -1.39352057, -2.48924933, -1.93094078, 3.26130366, 2.05692145]]
+yhat = model.predict(row)
+print('Prediction: %d' % yhat[0])
+
+# Gradient Boosting Machine for Regression
+# evaluate the model
+model = GradientBoostingRegressor()
+cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+n_scores = cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1, error_score='raise')
+print('MAE: %.3f (%.3f)' % (np.mean(n_scores), np.std(n_scores)))
+
+# fit the model on the whole dataset
+model = GradientBoostingRegressor()
+model.fit(X, y)
+# make a single prediction
+row = [[2.02220122, 0.31563495, 0.82797464, -0.30620401, 0.16003707, -1.44411381, 0.87616892, -0.50446586, 0.23009474, 0.76201118]]
+yhat = model.predict(row)
+print('Prediction: %.3f' % yhat[0])
